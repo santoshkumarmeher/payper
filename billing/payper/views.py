4f11a5fc85f00bc487ac4e1423e14721ec8d35ba
SANTOSH KUMAR MEHER <santoshkumarmeher13@gmail.com>
2023-12-02 10:16:19 +0530

    Added Login Page

--- billing/payper/views.py
+++ billing/payper/views.py
@@ -6,5 +6,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+def signin(request):
+    return render(request,'signin.html')
+def signup(request):
+    return render(request,'signup.html')
 
 
